c5ff978715eb191b0069a1c5ec8d1b25d115e227
Leandro Santana <96756735+Leandro-Santana013@users.noreply.github.com>
2025-03-17 04:19:59 -0300

    exFGTS

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("digite seu salário pra calcular o FGTS: ");
+double salario = double.Parse(Console.ReadLine());
+double fgts = salario * 0.08;
+Console.WriteLine($"O valor do FGTS é: {fgts}");
